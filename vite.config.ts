d6f4c2f6b24f012d9e1cc0ffc9d71cdc6dc12aef
Yadgarov <yadgarovfayzullo0@gmail.com>
2024-08-27 18:21:15 +0500

    logs deleted

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://cloud.umami.is", // Укажите URL вашего Umami сервера
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Если необходимо изменить путь
-      },
-    },
-  },
   plugins: [react()],
+  server: {
+    host: "127.0.0.2",
+    port: 3000,
+  },
+  build: {},
+  define: {
+    "process.env": {},
+  },
 });
