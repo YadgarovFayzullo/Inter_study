116d39f8675ecfda7f08d092ab6c5e1ad4b1a5e5
Yadgarov <yadgarovfayzullo0@gmail.com>
2025-01-31 22:48:30 +0500

    changed from 8080 to 3000

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,9 +1,9 @@
-export const ROUTE = "http://127.0.0.1:8080";
-export const ARCHIVE = "http://127.0.0.1:8080/archive";
-export const ARCHIVE_FILES = "http://127.0.0.1:8080";
-export const NEWS = "http://127.0.0.1:8080/news";
-export const COVER = "http://127.0.0.1:8080/files/cover";
-export const LOGIN = "http://127.0.0.1:8080/auth/login";
+export const ROUTE = "http://127.0.0.1:3000";
+export const ARCHIVE = "http://127.0.0.1:3000/archive";
+export const ARCHIVE_FILES = "http://127.0.0.1:3000";
+export const NEWS = "http://127.0.0.1:3000/news";
+export const COVER = "http://127.0.0.1:3000/files/cover";
+export const LOGIN = "http://127.0.0.1:3000/auth/login";
 // export const ROUTE = "https://api.interstudy.com.uz";
 // export const ARCHIVE = "https://api.interstudy.com.uz/archive";
 // export const ARCHIVE_FILES = "https://api.interstudy.com.uz";
